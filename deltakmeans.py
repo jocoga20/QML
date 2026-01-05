397b69be5651a03a88add84ca3a2fa8215bd843d
jocoga20 <gallicolijonathan@gmail.com>
2026-01-05 21:04:02 +0100

    delta k means e kmeans come classi

--- deltakmeans.py
+++ deltakmeans.py
@@ -1,45 +1,39 @@
 import numpy as np
 from random import choice
-import kmeans.compute_centroids
+from kmeans import KMeans
 
-def distance(x, y):
-    return np.linalg.norm(x-y)
-
-def assign_label(vector, centroids, delta):
-    k = centroids.shape[0]
-    d_min = float('inf')
-    filtered_centroids = []
-    distances = []
-
-    for i in range(k):
-        c = centroids[i]
-        d = distance(vector, c)
-        distances.append(d)
-        if d > d_min + delta:
-            continue
-        if d < d_min:
-            d_min = d
-        filtered_centroids.append(i)
+class DeltaKMeans(KMeans):
+    def __init__(self, delta, seed=42):
+        super().__init__()
+        self.delta = delta
+        self.seed = seed
     
-    filtered_centroids = [i for i in filtered_centroids if distances[i] <= d_min + delta]
-    return choice(filtered_centroids)
+    def assign_label(self, vector, centroids):
+        k = centroids.shape[0]
+        d_min = float('inf')
+        filtered_centroids = []
+        distances = []
 
-def compute_centroids(vectors, labels, k, delta):
-    d = vectors.shape[1]
-    return kmeans.compute_centroids(vectors, labels, k) + delta / (2 * d)
+        for i in range(k):
+            d = np.linalg.norm(vector - centroids[i])
+            distances.append(d)
+            print(d)
 
-def assign_labels(vectors, centroids, delta):
-    return np.array([assign_label(v, centroids, delta) for v in vectors])
+            if d > d_min + self.delta:
+                continue
 
-def deltakmeans(vectors, centroids, labels, delta, iterations=1, threshold=float('-inf')):
-    k = centroids.shape[0]
-    for _ in range(iterations):
-        labels = assign_labels(vectors, centroids, delta)
-        compute_centroids(vectors, labels, k, delta)
+            if d < d_min:
+                d_min = d
+
+            filtered_centroids.append(i)
         
+        filtered_centroids = [i for i in filtered_centroids if distances[i] <= d_min + self.delta]
+        return choice(filtered_centroids)
 
-vs = np.array([[0,0], [1,1], [2,2]])
-cs = np.array([[2,0], [2,1]])
-
-l = assign_label(vs[0], cs, delta=1e-10)
-print(l)
+    def assign_labels(self, vectors, centroids):
+        return np.array([self.assign_label(v, centroids, self.delta) for v in vectors])
+    
+    def update_centroids(self, vectors, labels, k):
+        d = vectors.shape[1]
+        return super().update_centroids(vectors, labels, k) + self.delta / (2 * d)
+    
--- kmeans.py
+++ kmeans.py
@@ -1,42 +1,33 @@
 import numpy as np
-from time import sleep
-from plot import plot2d
-from random import randint
 
-def assign_labels(vectors, centroids):
-    return np.array([np.linalg.norm(centroids - v, axis=1).argmin() for v in vectors])
-
-def update_centroids(vectors, labels, k):
-    return np.array([vectors[labels==label].mean(axis=0) for label in range(k)])
-
-def min_distance(vector, centroids):
-    return np.linalg.norm(vector - centroids, axis=1).min()
-
-def nearest_centroid_distances(vectors, centroids):
-    return np.array([min_distance(v, centroids) for v in vectors])
-
-def kmeanspp_init_centroids(vectors, k):
-    i = randint(0, vectors.shape[0]-1)
-    c0 = vectors[i]
+class KMeans:
+    def kmeanspp_init(self):
+        pass
     
-
-def kmeans(vectors, centroids, labels, iterations=1, threshold=float('-inf')):
-    """
-    :param vectors: n x d n = instances, d = feature size
-    :param centroids: k x d, k = num of different labels (given)
-    :param max_it: num of iterations required
-    :param threshold: when average movement of centroids is no longer bigger then threshold, algorithm stops. Default is -inf (no stop).
-
-    :returns: labels, centroids
-    """
-    k = centroids.shape[0]
-    threshold *= k
+    def assign_labels(self, vectors, centroids):
+        return np.array([np.linalg.norm(centroids - v, axis=1).argmin() for v in vectors])
     
-    for _ in range(iterations):
-        labels = labeling(vectors, centroids)
-        new_centroids = compute_centroids(vectors, labels, k)
-        if np.linalg.norm(new_centroids - centroids, axis=1).sum() <= threshold:
-            break
-        centroids = new_centroids
+    def update_centroids(self, vectors, labels, k):
+        return np.array([vectors[labels==label].mean(axis=0) for label in range(k)])
 
-    return labels, new_centroids
+    def run(self, vectors, centroids, labels, iterations=1, threshold=float('-inf')):
+        """
+        :param vectors: n x d n = instances, d = feature size
+        :param centroids: k x d, k = num of different labels (given)
+        :param max_it: num of iterations required
+        :param threshold: when average movement of centroids is no longer bigger then threshold, algorithm stops. Default is -inf (no stop).
+
+        :returns: labels, centroids
+        """
+        k = centroids.shape[0]
+        threshold *= k
+        
+        for _ in range(iterations):
+            labels = self.assign_labels(vectors, centroids)
+            new_centroids = self.update_centroids(vectors, labels, k)
+            if np.linalg.norm(new_centroids - centroids, axis=1).sum() <= threshold:
+                break
+            centroids = new_centroids
+
+        return labels, new_centroids
+    
