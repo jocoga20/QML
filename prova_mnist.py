480a102ae06c6ea10e4617eeb229a9d5bea1f96c
jocoga20 <gallicolijonathan@gmail.com>
2026-01-08 21:29:57 +0100

    accuracy

--- prova_mnist.py
+++ prova_mnist.py
@@ -3,29 +3,40 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from torchvision import transforms, datasets
 from deltakmeans import DeltaKMeans
+from kmeans import KMeans
 
 def get_mnist(train):
     m = datasets.MNIST('./data', train=train, transform=transforms.ToTensor(), download=True)
     return m.data.numpy().reshape(-1, 784), m.targets.numpy()
-
+d = 40
+"""
 X_tr, y_tr = get_mnist(True)
 X_ts, y_ts = get_mnist(False)
 
 scaler = StandardScaler()
 X_tr = scaler.fit_transform(X_tr)
 X_ts = scaler.transform(X_ts)
+"""
+"""
+X_tr = np.load('x.train.std.npy')
+X_ts = np.load('x.test.std.npy')
 
-d = 40
 pca = PCA(d)
 X_tr = pca.fit_transform(X_tr)
 X_ts = pca.transform(X_ts)
+"""
 
-k = 10
+X_tr = np.load('x.train.40.npy')
+X_ts = np.load('x.test.40.npy')
+y_tr = np.load('y.train.npy')
+y_ts = np.load('y.test.npy')
 
+k = int(d / 2)
+km = KMeans()
 km = DeltaKMeans(delta=.5)
 c = km.kmeanspp_init(vectors=X_tr, k=k)
 
-labels, centroids = km.run(vectors=X_tr, centroids=c, labels=np.zeros(shape=X_tr.shape[0]), max_it=100, threshold=1e-4)
+labels, centroids = km.run(vectors=X_tr, centroids=c, labels=np.zeros(shape=X_tr.shape[0]), max_it=50, threshold=1e-4)
 
 def get_centroids_majority_class(labels, y, k):
     return [np.bincount(y[labels == i]).argmax() for i in range(k)]
@@ -35,14 +46,9 @@ def nearest_centroid_index(vector, centroids):
 
 majority_class_by_centroid = get_centroids_majority_class(labels, y_tr, k)
 
-accuracy = 0.
-preds = []
-for x in X_ts:
-    i = nearest_centroid_index(x, centroids)
-    pred = majority_class_by_centroid[i]
-    preds.append(pred)
+def accuracy(X, y, centroids):
+    preds = np.array([majority_class_by_centroid[nearest_centroid_index(x, centroids)] for x in X])
+    return (preds == y).sum() / X.shape[0]
 
-preds = np.array(preds)
-accuracy = (preds == y_ts).sum() / X_ts.shape[0]
-
-print(accuracy)
+print(f'TRAIN: {accuracy(X_tr, y_tr, centroids)}')
+print(f'TEST: {accuracy(X_ts, y_ts, centroids)}')
